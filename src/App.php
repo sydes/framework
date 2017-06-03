d15778b60195c13d8c1c1643b6b854be9e78af59
Arthur Grand <artygrand@gmail.com>
2017-06-03 18:57:50 +0700

    Added blank App

---
+++ src/App.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @link      https://github.com/sydes/framework
+ * @copyright 2011-2017, ArtyGrand <artygrand.ru>
+ * @license   MIT License; see LICENSE.md
+ */
+
+namespace Sydes;
+
+class App
+{
+
+}
